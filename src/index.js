6c7fc7f8d406002cd37da73d81964c73b5f6670e
Konstantin F <kf3329730@gmail.com>
2024-07-30 01:59:07 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
+  const stringNumber = String(n);
+  const getReverse = stringNumber.split('').reverse();
+  let result = [];
+  for(let i = 0; i < getReverse.length; i += 1) {
+    if(!(Number(getReverse[i] === 0) || isNaN(getReverse[i]))) {
+      result.push(getReverse[i]);
+    }
+  }
+  return Number(result.join(''));
   
 }
